66c0ccde053d0da07223867f6400d7ca672972e6
bjsmith <brian-j-smith@uiowa.edu>
2018-08-23 17:46:54 -0500

    Optimize response extraction

--- R/resample.R
+++ R/resample.R
@@ -8,9 +8,9 @@ setGeneric(".resample", function(object, ...) standardGeneric(".resample"))
 
 setMethod(".resample", "CVControl",
   function(object, formula, data, model) {
-    foldids <- model.frame(formula, data, na.action = NULL) %>%
-      model.response() %>%
-      createMultiFolds(k = object@folds, times = object@repeats)
+    foldids <- createMultiFolds(eval(formula[[2]], data),
+                                k = object@folds,
+                                times = object@repeats)
     foreach(foldid = foldids,
             .packages = c("survival", "MLModels"),
             .combine = "rbind") %dopar% {
@@ -26,7 +26,7 @@ validate <- function(formula, train, test, model, control) {
   mfit <- fit(formula, train, model)
   pred <- model@predict(object = mfit, data = test, type = "prob",
                         times = control@survtimes)
-  obs <- model.response(model.frame(formula, test, na.action = NULL))
+  obs <- eval(formula[[2]], test)
   do.call(control@summary,
           c(list(observed = obs, predicted = pred), as(control, "list")))
 }
