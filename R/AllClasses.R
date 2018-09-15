7f4479d0d777666c36f3b065f84cc254ee8e1dc3
bjsmith <brian-j-smith@uiowa.edu>
2018-09-15 10:10:08 -0500

    Save show methods in a separate file

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -158,21 +158,6 @@ MLModel <- function(name = "MLModel", packages = character(0),
 }
 
 
-setMethod("show", "MLModel",
-  function(object) {
-    cat("An object of class \"", class(object), "\"\n\n",
-        "name: ", object@name, "\n\n",
-        "packages: ", toString(object@packages), "\n\n",
-        "types: ", toString(object@types), "\n\n",
-        "params:\n",
-        sep = "")
-    print(object@params)
-    if(length(object@params) == 0) cat("\n")
-    invisible()
-  }
-)
-
-
 setClass("Resamples",
   slots = c("method" = "character"),
   contains = "array"
@@ -233,37 +218,9 @@ setMethod("initialize", "Resamples",
 )
 
 
-setMethod("show", "Resamples",
-  function(object) {
-    cat("An object of class \"", class(object), "\"\n\n", sep = "")
-    dns <- dimnames(object)
-    if(length(dns) > 2) cat("models: ", toString(dns[[3]]), "\n\n", sep = "")
-    cat("metrics: ", toString(dns[[2]]), "\n\n",
-        "method: ", object@method, "\n\n",
-        "resamples: ", dim(object)[1], "\n\n", sep = "")
-    invisible()
-  }
-)
-
-
 MLModelTune <- setClass("MLModelTune",
   slots = c(grid = "data.frame", resamples = "Resamples", selected = "numeric"),
   contains = "MLModel"
-)
-
-
-setMethod("show", "MLModelTune",
-  function(object) {
-    callNextMethod(object)
-    cat("grid:\n")
-    print(object@grid)
-    cat("\nresamples:\n")
-    print(object@resamples)
-    if(length(dim(object@resamples)) > 2) {
-      cat("selected: Model", object@selected, " (", names(object@selected),
-          ")\n\n", sep = "")
-    }
-  }
 )
 
 
@@ -285,18 +242,6 @@ setMethod("initialize", "ResamplesDiff",
 ResamplesHTest <- setClass("ResamplesHTest",
   slots = c("adjust" = "character"),
   contains = "array"
-)
-
-
-setMethod("show", "ResamplesHTest",
-  function(object) {
-    cat("An object of class \"", class(object), "\"\n\n",
-        "upper diagonal: mean differences (row - column)\n",
-        "lower diagonal: p-values\n",
-        "p-value adjustment: ", object@adjust, "\n\n",
-        sep = "")
-    print(object@.Data)
-  }
 )
 
 
---
+++ R/show.R
@@ -0,0 +1,53 @@
+setMethod("show", "MLModel",
+  function(object) {
+    cat("An object of class \"", class(object), "\"\n\n",
+        "name: ", object@name, "\n\n",
+        "packages: ", toString(object@packages), "\n\n",
+        "types: ", toString(object@types), "\n\n",
+        "params:\n",
+        sep = "")
+    print(object@params)
+    if(length(object@params) == 0) cat("\n")
+    invisible()
+  }
+)
+
+
+setMethod("show", "MLModelTune",
+  function(object) {
+    callNextMethod(object)
+    cat("grid:\n")
+    print(object@grid)
+    cat("\nresamples:\n")
+    print(object@resamples)
+    if(length(dim(object@resamples)) > 2) {
+      cat("selected: Model", object@selected, " (", names(object@selected),
+          ")\n\n", sep = "")
+    }
+  }
+)
+
+
+setMethod("show", "Resamples",
+  function(object) {
+    cat("An object of class \"", class(object), "\"\n\n", sep = "")
+    dns <- dimnames(object)
+    if(length(dns) > 2) cat("models: ", toString(dns[[3]]), "\n\n", sep = "")
+    cat("metrics: ", toString(dns[[2]]), "\n\n",
+        "method: ", object@method, "\n\n",
+        "resamples: ", dim(object)[1], "\n\n", sep = "")
+    invisible()
+  }
+)
+
+
+setMethod("show", "ResamplesHTest",
+  function(object) {
+    cat("An object of class \"", class(object), "\"\n\n",
+        "upper diagonal: mean differences (row - column)\n",
+        "lower diagonal: p-values\n",
+        "p-value adjustment: ", object@adjust, "\n\n",
+        sep = "")
+    print(object@.Data)
+  }
+)
