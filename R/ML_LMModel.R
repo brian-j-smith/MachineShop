20b89331f42760e49bf40309c815c22648075a25
bjsmith <brian-j-smith@uiowa.edu>
2018-12-03 03:41:52 -0600

    Simplify LMModel implementation

--- R/ML_LMModel.R
+++ R/ML_LMModel.R
@@ -29,24 +29,24 @@ LMModel <- function() {
     fit = function(formula, data, weights, ...) {
       environment(formula) <- environment()
       y <- response(formula, data)
-      y_name <- response(terms(formula))
-      if (is_response(y, "binary")) {
-        y <- y == levels(y)[2]
-        data[[y_name]] <- y
+      if (is.factor(y)) {
+        y_name <- response(terms(formula))
         formula[[2]] <- as.symbol(y_name)
-      } else if (is(y, "factor")) {
-        mm <- model.matrix(~ y - 1)
-        colnames(mm) <- levels(y)
-        data[[y_name]] <- mm
-        formula[[2]] <- as.symbol(y_name)
+        if (nlevels(y) == 2) {
+          y <- y == levels(y)[2]
+          data[[y_name]] <- y
+        } else {
+          mm <- model.matrix(~ y - 1)
+          colnames(mm) <- levels(y)
+          data[[y_name]] <- mm
+        }
       }
-      args <- list(formula = formula, data = data, ...)
-      if (is(y, "numeric")) {
-        args$weights <- weights
+      if (is.numeric(y)) {
+        stats::lm(formula, data = data, weights = weights, ...)
       } else {
         assert_equal_weights(weights)
+        stats::lm(formula, data = data, ...)
       }
-      do.call(stats::lm, args)
     },
     predict = function(object, newdata, ...) {
       predict(object, newdata = newdata)
