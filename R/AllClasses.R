a8a1d1949c7d6d509d778b9675c9c11fb7193ee2
Brian J Smith <brian-j-smith@uiowa.edu>
2018-12-28 18:04:36 -0600

    Documentation edits: MLModel argument details

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -234,49 +234,68 @@ setClass("MLMetric",
 #' Create a model for use with the \pkg{MachineShop} package.
 #' 
 #' @param name character string name for the instantiated \code{MLModel} object;
-#' same as the derived model constructor name.
+#' same name as the object to which the model is assigned.
 #' @param label descriptive label for the model.
-#' @param packages character vector of packages required by the object.
-#' @param types character vector of response variable types on which the model
-#' can be fit.
-#' @param params list of user-specified model parameters.
-#' @param nvars function to return the number of predictor variables for a
-#' given model frame.
-#' @param fit model fitting function.
-#' @param predict model prediction function.
-#' @param varimp variable importance function.
+#' @param packages character vector of packages whose namespaces are required by
+#' the model.
+#' @param types character vector of response variable types to which the model
+#' can be fit.  Supported types are \code{"binary"}, \code{"factor"},
+#' \code{"matrix"}, \code{"numeric"}, \code{"ordered"}, and \code{"Surv"}.
+#' @param params list of user-specified model parameters to be passed to the
+#' \code{fit} function.
+#' @param nvars function to determine the number of variables included in the
+#' model from the \code{data} frame used by \code{fit}.
+#' @param fit model fitting function whose arguments are a \code{formula}, a
+#' \code{data} frame, case \code{weights}, and an ellipsis (\code{...}).
+#' @param predict model prediction function whose arguments are the
+#' \code{object} returned by \code{fit}, a \code{newdata} frame of predictor
+#' variables, optional vector of \code{times} at which to predict survival,
+#' and an ellipsis.
+#' @param varimp variable importance function whose arguments are the
+#' \code{object} returned by \code{fit}, optional arguments passed from calls
+#' to \code{\link{varimp}}, and an ellipsis.
 #' 
-#' @details \code{MLModel} can be used to create custom model constructors.  To
-#' ensure full compatibility with the \pkg{MachineShop} package, add derived
-#' constructor functions to the package namespace with the \code{environment}
-#' function.  See example below.
+#' @details
+#' Values returned by the \code{predict} functions should be formatted according
+#' to the response variable types below.
+#' \describe{
+#' \item{factor}{a vector or column matrix of probabilities for the second level
+#' of binary factors or a matrix whose columns contain the probabilities for
+#' factors with more than two levels.}
+#' \item{matrix}{a matrix of predicted responses.}
+#' \item{numeric}{a vector or column matrix of predicted responses.}
+#' \item{Surv}{a matrix whose columns contain survival probabilities at
+#' \code{times} if supplied or a vector of survival predictions otherwise.}
+#' }
+#' 
+#' The \code{varimp} function should return a vector of importance values named
+#' after the predictor variables or a matrix or data frame whose rows are named
+#' after the predictors.
 #' 
 #' @return \code{MLModel} class object.
 #' 
-#' @seealso \code{\link{modelinfo}}
+#' @seealso \code{\link{modelinfo}}, \code{\link{fit}}, \code{\link{resample}},
+#' \code{\link{tune}}
 #' 
 #' @examples
-#' ## Logistic regression model constructor
-#' LogisticModel <- function() {
-#'   MLModel(
-#'     name = "LogisticModel",
-#'     label = "Logistic Regression",
-#'     package = "stats",
-#'     types = "binary",
-#'     fit = function(formula, data, weights, ...) {
-#'       stats::glm(formula, data = data, weights = weights, family = binomial, ...)
-#'     },
-#'     predict = function(object, newdata, ...) {
-#'       predict(object, newdata = newdata, type = "response")
-#'     },
-#'     varimp = function(object, ...) {
-#'       pchisq(coef(object)^2 / diag(vcov(object)), 1)
-#'     }
-#'   )
-#' }
+#' ## Logistic regression model
+#' LogisticModel <- MLModel(
+#'   name = "LogisticModel",
+#'   types = "binary",
+#'   fit = function(formula, data, weights, ...) {
+#'     glm(formula, data = data, weights = weights, family = binomial, ...)
+#'   },
+#'   predict = function(object, newdata, ...) {
+#'     predict(object, newdata = newdata, type = "response")
+#'   },
+#'   varimp = function(object, ...) {
+#'     pchisq(coef(object)^2 / diag(vcov(object)), 1)
+#'   }
+#' )
 #' 
-#' ## Add to MachineShop for full compatibility
-#' environment(LogisticModel) <- asNamespace("MachineShop")
+#' library(MASS)
+#' res <- resample(type ~ ., data = Pima.tr, model = LogisticModel)
+#' summary(res)
 #' 
 MLModel <- function(name = "MLModel", label = name, packages = character(0),
                     types = character(0), params = list(),
