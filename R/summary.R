aa3ac85a9f12bd3cf1ce5110ee03b3bec8bd1419
bjsmith <brian-j-smith@uiowa.edu>
2019-01-13 22:27:22 -0600

    Preserve summary stats names if na.omit all values

--- R/summary.R
+++ R/summary.R
@@ -29,7 +29,7 @@ summary.Performance <- function(object,
 
   f <- function(x) {
     prop_na <- mean(is.na(x))
-    if (na.rm) x <- na.omit(x)
+    if (na.rm) x <- as.numeric(na.omit(x))
     c(stats(x), "NA" = prop_na)
   }
   
