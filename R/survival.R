486ddf9edde431ae5904bd3d20eded24203228db
bjsmith <brian-j-smith@uiowa.edu>
2019-01-25 15:31:39 -0600

    Code edits: unname basehaz vector

--- R/survival.R
+++ R/survival.R
@@ -2,7 +2,7 @@ basehaz <- function(y, risk, times) {
   y_times <- unique(y[, "time"]) %>% sort
   nrisk <- rowsum(risk, y[, "time"]) %>% rev %>% cumsum %>% rev
   nevent <- rowsum(y[, "status"], y[, "time"])[, 1]
-  cumhaz <- cumsum(nevent / nrisk) %>% structure(names = NULL)
+  cumhaz <- cumsum(nevent / nrisk) %>% unname
   idx <- approx(y_times, seq_along(y_times), times, method = "constant",
                 f = 0, yleft = 0, yright = length(y_times))$y
   c(0, cumhaz)[idx + 1]
