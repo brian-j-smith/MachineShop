0ba4c227bd4296eef65df5a9704a3c6e7361b991
bjsmith <brian-j-smith@uiowa.edu>
2019-04-22 09:59:34 -0500

    Code edits: move setAs.R functions to MLModel.R

--- R/MLModel.R
+++ R/MLModel.R
@@ -108,3 +108,43 @@ MLModel <- function(name = "MLModel", label = name, packages = character(),
 MLFitBits <- function(...) {
   new("MLFitBits", ...)
 }
+
+
+asMLModelFit <- function(object, Class, model, x, y) {
+  fitbits <- model@fitbits
+  fitbits@x <- x
+  fitbits@y <- y
+
+  if (is(object, Class)) {
+    object <- unMLModelFit(object)
+  } else if (is(object, "MLModelFit")) {
+    stop("cannot change MLModelFit class")
+  }
+  
+  if (!is(model, "MLModel")) stop("model not of class MLModel")
+  
+  if (isS4(object)) {
+    object <- new(Class, object, fitbits = fitbits)
+  } else if (is.list(object)) {
+    object$fitbits <- fitbits
+    class(object) <- c(Class, "MLModelFit", class(object))
+  } else {
+    stop("unsupported object class")
+  }
+
+  object
+}
+
+
+unMLModelFit <- function(object) {
+  if (!is(object, "MLModelFit")) return(object)
+  if (isS4(object)) {
+    classes <- extends(class(object))
+    as(object, classes[match("MLModelFit", classes) + 1])
+  } else {
+    object$fitbits <- NULL
+    classes <- class(object)
+    pos <- match("MLModelFit", classes)
+    structure(object, class = classes[-c(pos - 1, pos)])
+  }
+}
