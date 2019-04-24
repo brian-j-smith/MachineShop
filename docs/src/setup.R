d704d039f9f5659c7e01eda6c6a3c914725a6cae
bjsmith <brian-j-smith@uiowa.edu>
2019-04-24 00:15:33 -0500

    Doc edits: add IC Home and Pima Indians examples

--- docs/src/setup.R
+++ docs/src/setup.R
@@ -8,8 +8,9 @@ knitr::opts_chunk$set(
   fig.align = "center"
 )
 
+
 library(kableExtra)
-library(ggplot2)
+
 
 rdoc_url <- function(name) {
   version <- packageVersion("MachineShop")
@@ -17,4 +18,42 @@ rdoc_url <- function(name) {
               "/", version$major, ".", version$minor, ".0",
               "/topics/")
   paste0("[`", name, "`](", url, name, ")")
+}
+
+
+median_range <- function(x, prefix = NULL, ...) {
+  formatted_median <- format(median(x), trim = TRUE, ...)
+  formatted_range <- format(range(x), trim = TRUE, ...)
+  paste0(prefix, formatted_median,
+         " (", toString(paste0(prefix, formatted_range)), ")")
+}
+
+
+n_perc <- function(x, digits = 2) {
+  paste0(sum(x), " (", round(100 * mean(x), digits), "%)")
+}
+
+
+summary_kbl <- function(x, data) {
+  vals <- sapply(unlist(unname(x), recursive = FALSE), function(el) {
+    eval(el[[2]], envir = data)
+  })
+  
+  kbl <- data.frame(Characteristic = names(vals), Value = vals) %>%
+    kable(align = c("l", "c")) %>%
+    kable_styling(c("striped", "condensed"), full_width = FALSE,
+                  position = "center")
+
+  start_row <- 1
+  for (i in seq(x)) {
+    group_label <- names(x)[i]
+    group_length <- length(x[[i]])
+    if (nzchar(group_label)) {
+      kbl <- group_rows(kbl, group_label, start_row,
+                        start_row + group_length - 1)
+    }
+    start_row <- start_row + group_length
+  }
+  
+  kbl
 }
