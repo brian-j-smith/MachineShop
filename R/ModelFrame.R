a179ed011eeeb5171f6e74c8a887568c948b7123
bjsmith <brian-j-smith@uiowa.edu>
2019-04-26 12:56:50 -0500

    Use backticks for non-standard predictor names in terms formulas

--- R/ModelFrame.R
+++ R/ModelFrame.R
@@ -165,33 +165,38 @@ valid_predictor_calls <- c(
 
 terms.character <- function(x, y = NULL, intercept = TRUE, all_numeric = FALSE,
                             ...) {
+  x_names <- x
+  x <- lapply(x, as.symbol)
+  
   if (is.character(y)) y <- as.symbol(y)
   y_indicator <- 1L - is.null(y)
   
   fo <- parse(text = paste(
     "y ~",
-    paste(if (length(x)) x else if (intercept) "1", collapse = "+"),
+    if (length(x)) {
+      paste(sapply(x, deparse, backtick = TRUE), collapse = "+")
+    } else if (intercept) "1",
     if (!intercept) "- 1"
   ))[[1]]
   fo[[2]] <- y
   
-  all_vars <- c(if (y_indicator) deparse(y), x)
+  var_names <- c(if (y_indicator) deparse(y), x_names)
   
   if (all_numeric) {
     class <- "DesignTerms"
     factors <- cbind(rep(c(0L, 1L), c(y_indicator, length(x))))
-    rownames(factors) <- all_vars
+    rownames(factors) <- var_names
   } else {
     class <- "FormulaTerms"
     factors <- rbind(rep(0L, y_indicator * length(x)), diag(1L, length(x)))
-    dimnames(factors) <- list(all_vars, x)
+    dimnames(factors) <- list(var_names, x_names)
   }
   
   structure(
     fo,
-    variables = as.call(c(quote(list), y, lapply(x, as.symbol))),
+    variables = as.call(c(quote(list), y, x)),
     factors = factors,
-    term.labels = x,
+    term.labels = x_names,
     order = rep(1L, length(x)),
     intercept = as.integer(intercept),
     response = y_indicator,
