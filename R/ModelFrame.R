22fedac9830ee9fb0ee0a3cbd194bf35337d9ad0
bjsmith <brian-j-smith@uiowa.edu>
2020-03-05 14:25:24 -0600

    Pass missing values in model.matrix FormulaTerms method

--- R/ModelFrame.R
+++ R/ModelFrame.R
@@ -358,7 +358,9 @@ model.matrix.DesignTerms <- function(object, data, ...) {
 
 
 model.matrix.FormulaTerms <- function(object, data, ...) {
-  model.matrix.default(delete.response(object), as.data.frame(data), ...)
+  fo <- delete.response(object)
+  mf <- model.frame(fo, data, na.action = na.pass)
+  model.matrix.default(fo, mf, ...)
 }
 
 
