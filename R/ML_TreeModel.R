249fc8b249d1b4a9e0ea2f632cbae33455ffc24a
bjsmith <brian-j-smith@uiowa.edu>
2020-03-12 13:04:51 -0500

    Add cost-complexity pruning parameters to TreeModel

--- R/ML_TreeModel.R
+++ R/ML_TreeModel.R
@@ -8,6 +8,11 @@
 #' @param mindev within-node deviance must be at least this times that of the
 #'   root node for the node to be split.
 #' @param split splitting criterion to use.
+#' @param k scalar cost-complexity parameter defining a subtree to return.
+#' @param best integer alternative to \code{k} requesting the number of terminal
+#'   nodes of a subtree in the cost-complexity sequence to return.
+#' @param method character string denoting the measure of node heterogeneity
+#'   used to guide cost-complexity pruning.
 #'
 #' @details
 #' \describe{
@@ -18,16 +23,18 @@
 #'
 #' @return \code{MLModel} class object.
 #'
-#' @seealso \code{\link[tree]{tree}}, \code{\link{fit}},
-#' \code{\link{resample}}
+#' @seealso \code{\link[tree]{tree}}, \code{\link[tree]{prune.tree}},
+#' \code{\link{fit}}, \code{\link{resample}}
 #'
 #' @examples
 #' fit(Species ~ ., data = iris, model = TreeModel)
 #'
 TreeModel <- function(mincut = 5, minsize = 10, mindev = 0.01,
-                      split = c("deviance", "gini")) {
+                      split = c("deviance", "gini"), k = NULL, best = NULL,
+                      method = c("deviance", "misclass")) {
 
   split <- match.arg(split)
+  method <- if (is.null(k) && is.null(best)) NULL else match.arg(method)
 
   MLModel(
     name = "TreeModel",
@@ -36,9 +43,13 @@ TreeModel <- function(mincut = 5, minsize = 10, mindev = 0.01,
     response_types = c("factor", "numeric"),
     predictor_encoding = "terms",
     params = params(environment()),
-    fit = function(formula, data, weights, split, ...) {
-      tree::tree(formula, data = as.data.frame(data), weights = weights,
-                 split = split, ...)
+    fit = function(formula, data, weights, split, k = NULL, best = NULL,
+                   method = NULL, ...) {
+      modelfit <- tree::tree(formula, data = as.data.frame(data),
+                             weights = weights, split = split, ...)
+      if (!is.null(method)) {
+        tree::prune.tree(modelfit, k = k, best = best, method = method)
+      } else modelfit
     },
     predict = function(object, newdata, times, ...) {
       newdata <- as.data.frame(newdata)
