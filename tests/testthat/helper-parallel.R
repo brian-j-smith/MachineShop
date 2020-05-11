3afbce23c8275388896bbd579ad4b3ae77151871
bjsmith <brian-j-smith@uiowa.edu>
2020-05-11 18:49:47 -0500

    Revert "Switch docs and tests from doParallel to doSNOW"
    This reverts commit 8652756f4c3b6672c627564575b00925d517163c.

--- tests/testthat/helper-parallel.R
+++ tests/testthat/helper-parallel.R
@@ -1,10 +1,9 @@
 with_parallel <- function(code) {
   cores <- as.numeric(Sys.getenv("TEST_CORES"))
   if (is.na(cores)) cores <- 1
-  if (require("doSNOW", quietly = TRUE) && cores > 1) {
-    cl <- makeCluster(cores)
-    doSNOW::registerDoSNOW(cl)
-    on.exit(stopCluster(cl))
+  if (require("doParallel", quietly = TRUE) && cores > 1) {
+    doParallel::registerDoParallel(cores = cores)
+    on.exit(doParallel::stopImplicitCluster())
   }
 
   code
