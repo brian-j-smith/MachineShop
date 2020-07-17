afca775ece0423bf270d13a1670a654127c29cc4
bjsmith <brian-j-smith@uiowa.edu>
2020-07-17 09:05:51 -0500

    Refactor step_kmedoids as subclass of step_sbf
    Support user-specified center and scale functions
    Append prefix to selected variable names
    Fix tidy() output of silhouette column
    Rename tidy() column medoids to selected
    Rename tidy() column names to name
    Set tidy() non-selected variable names to NA

--- R/step_kmedoids.R
+++ R/step_kmedoids.R
@@ -3,13 +3,14 @@
 #' Creates a \emph{specification} of a recipe step that will partition numeric
 #' variables according to k-medoids clustering and select the cluster medoids.
 #'
-#' @inheritParams step_kmeans
+#' @inheritParams step_sbf
 #' @param k number of k-medoids clusterings of the variables.  The value of
 #'   \code{k} is constrained to be between 1 and one less than the number of
 #'   original variables.
 #' @param center,scale logicals indicating whether to mean center and median
 #'   absolute deviation scale the original variables prior to cluster
-#'   partitioning; not applied to selected variables.
+#'   partitioning, or functions or names of functions for the centering and
+#'   scaling; not applied to selected variables.
 #' @param method character string specifying one of the clustering methods
 #'   provided by the \pkg{cluster} package.  The \code{clara} (clustering
 #'   large applications) method is an extension of \code{pam} (partitioning
@@ -22,18 +23,16 @@
 #' @param num_samp number of sub-datasets to sample for the
 #'   \code{\link[cluster]{clara}} clustering method.
 #' @param samp_size number of cases to include in each sub-dataset.
-#' @param prefix if the original variables are not replaced, a character string
-#'   prefix added to a sequence of zero-padded integers to generate names for
-#'   the resulting new variables; otherwise, the original variable names are
-#'   retained.
 #' @param x \code{step_kmedoids} object.
 #'
-#' @return An updated version of \code{recipe} with the new step added to the
-#' sequence of existing steps (if any).  For the \code{tidy} method, a tibble
-#' with columns \code{terms} (selectors or variables selected), \code{cluster}
-#' assignments, \code{medoid} (logical indicator of cluster medoids),
-#' \code{silhouette} (silhouette values), and \code{names} for the new
-#' variables.
+#' @return Function \code{step_kmedoids} creates a new step whose class is of
+#' the same name and inherits from \code{\link{step_sbf}}, adds it to the
+#' sequence of existing steps (if any) in the recipe, and returns the updated
+#' recipe.  For the \code{tidy} method, a tibble with columns \code{terms}
+#' (selectors or variables selected), \code{cluster} assignments,
+#' \code{selected} (logical indicator of selected cluster medoids),
+#' \code{silhouette} (silhouette values), and \code{name} of the selected
+#' variable names.
 #'
 #' @details
 #' K-medoids clustering partitions variables into k groups such that the
@@ -67,142 +66,106 @@
 #' tidy(kmedoids_prep, number = 1)
 #'
 step_kmedoids <- function(recipe, ..., k = 5, center = TRUE, scale = TRUE,
-                          method = c("pam", "clara"),
-                          metric = "euclidean", optimize = FALSE,
-                          num_samp = 50, samp_size = 40 + 2 * k,
-                          replace = TRUE, prefix = "KMedoids",
-                          role = "predictor", skip = FALSE,
-                          id = recipes::rand_id("kmedoids")) {
+                          method = c("pam", "clara"), metric = "euclidean",
+                          optimize = FALSE, num_samp = 50,
+                          samp_size = 40 + 2 * k, replace = TRUE,
+                          prefix = "KMedoids", role = "predictor",
+                          skip = FALSE, id = recipes::rand_id("kmedoids")) {
 
-  requireModelNamespaces("cluster")
-
-  step <- recipes::step(
-    subclass = "kmedoids",
+  recipes::add_step(recipe, new_step_kmedoids(
     terms = recipes::ellipse_check(...),
     k = k,
-    center = if (isTRUE(center)) base::mean else FALSE,
-    scale = if (isTRUE(scale)) stats::mad else FALSE,
+    center = center,
+    scale = scale,
     method = match.arg(method),
-    res = list(),
-    trained = FALSE,
+    metric = metric,
+    optimize = optimize,
+    num_samp = num_samp,
+    samp_size = samp_size,
     replace = replace,
-    prefix = if (!replace) prefix,
+    prefix = prefix,
     role = role,
     skip = skip,
     id = id
-  )
-  switch(step$method,
-         "pam" = {
-           step$metric <- match.arg(metric, c("euclidean", "manhattan"))
-           step$optimize <- optimize
-         },
-         "clara" = {
-           step$metric <- match.arg(metric,
-                                    c("euclidean", "manhattan", "jaccard"))
-           step$num_samp <- num_samp
-           step$samp_size <- samp_size
-         })
-  recipes::add_step(recipe, step)
+  ))
 
 }
 
 
-prep.step_kmedoids <- function(x, training, info = NULL, ...) {
+new_step_kmedoids <- function(..., k, center, scale, method, metric, optimize,
+                              num_samp, samp_size) {
 
   requireModelNamespaces("cluster")
 
-  col_names <- recipes::terms_select(terms = x$terms, info = info)
-  if (length(col_names) < 2) {
-    stop("step_kmedoids requires 2 or more variables", call. = FALSE)
-  }
+  filter <- function(x, y, step) {
 
-  training <- training[col_names]
-  recipes::check_type(training)
+    requireModelNamespaces("cluster")
 
-  x$k <- max(min(x$k, length(col_names) - 1), 1)
+    if (ncol(x) < 2) {
+      stop("step_kmedoids requires 2 or more variables", call. = FALSE)
+    }
+    recipes::check_type(x)
 
-  if (is.function(x$center)) x$center <- apply(training, 2, x$center)
-  if (is.function(x$scale)) x$scale <- apply(training, 2, x$scale)
-  training <- t(scale(training, center = x$center, scale = x$scale))
-
-  res <- switch(x$method,
-                "pam" = {
-                  keep <- c("id.med", "clustering", "sil_width")
-                  cluster::pam(training, k = x$k, metric = x$metric,
-                               pamonce = x$optimize, keep.diss = FALSE,
-                               keep.data = FALSE)[keep]
-                },
-                "clara" = {
-                  x$samp_size <- min(x$samp_size, nrow(training))
-                  keep <- c("i.med", "clustering", "sil_width")
-                  cluster::clara(training, k = x$k, metric = x$metric,
-                                 samples = x$num_samp, sampsize = x$samp_size,
-                                 medoids.x = FALSE, rngR = TRUE)[keep]
-                })
-  names(res) <- c("id.med", "clustering", "sil_width")
-
-  names(res$id.med) <- if (x$replace) {
-    names(res$clustering)[res$id.med]
-  } else {
-    recipes::names0(length(res$id.med), x$prefix)
-  }
-
-  x$res <- res[c("id.med", "clustering")]
-  x$silhoutte <- res$silinfo$widths[, "sil_width"]
-  x$trained <- TRUE
-  x
-
-}
-
-
-bake.step_kmedoids <- function(object, new_data, ...) {
-  res <- object$res
-  var_names <- names(res$clustering)
-  if (object$replace) {
-    names_drop <- var_names[-res$id.med]
-    new_data[!(names(new_data) %in% names_drop)]
-  } else {
-    cluster_medoids <- recipes::check_name(
-      new_data[var_names[res$id.med]], new_data, object,
-      newname = names(res$id.med)
+    stats <- map(function(stat) {
+      if (is.function(stat)) apply(x, 2, stat) else stat
+    }, step[c("center", "scale")])
+    x <- t(base::scale(x, center = stats$center, scale = stats$scale))
+    k <- max(min(step$k, nrow(x) - 1), 1)
+    switch(
+      step$method,
+      "pam" = {
+        res <- cluster::pam(x, k, metric = step$metric, pamonce = step$optimize,
+                            keep.diss = FALSE, keep.data = FALSE)
+      },
+      "clara" = {
+        samp_size <- min(step$samp_size, nrow(x))
+        res <- cluster::clara(x, k, metric = step$metric,
+                              samples = step$num_samp, sampsize = samp_size,
+                              medoids.x = FALSE, rngR = TRUE)
+        names(res)[names(res) == "i.med"] <- "id.med"
+      }
     )
-    as_tibble(c(new_data, cluster_medoids))
-  }
-}
 
-
-print.step_kmedoids <- function(x, width = max(20, options()$width - 33), ...) {
-  cat("K-medoids cluster extraction for ")
-  recipes::printer(names(x$res$clustering), x$terms, x$trained, width = width)
-  invisible(x)
-}
-
-
-#' @rdname step_kmedoids
-#'
-tidy.step_kmedoids <- function(x, ...) {
-  cluster <- x$res$clustering
-  medoid_ind <- x$res$id.med
-  if (is.trained(x)) {
-    res <- tibble(
-      terms = names(cluster),
-      cluster = cluster,
-      medoid = FALSE
+    tibble(
+      cluster = res$clustering,
+      selected = seq_along(res$clustering) %in% res$id.med,
+      silhoutte = if (k == 1) NA_real_ else
+        res$silinfo$widths[names(res$clustering), "sil_width"]
     )
-    res$medoid[medoid_ind] <- TRUE
-    res$silhouette <- x$res$silhouette[res$terms]
-    res$names <- names(medoid_ind)[cluster]
-  } else {
-    res <- tibble(
-      terms = recipes::sel2char(x$terms),
-      cluster = NA_integer_,
-      medoid = NA,
-      silhouette = NA_real_,
-      names = NA_character_
-    )
+
   }
-  res$id <- x$id
-  res
+
+  object <- new_step_sbf(..., filter = filter, multivariate = TRUE)
+
+  object$k <- k
+  object$center <- if (!is.logical(center)) fget(center) else
+    if (center) base::mean else FALSE
+  object$scale <- if (!is.logical(scale)) fget(scale) else
+    if (scale) stats::mad else FALSE
+  object$method <- method
+  switch(
+    object$method,
+    "pam" = {
+      object$metric <- match.arg(metric, c("euclidean", "manhattan"))
+      object$optimize <- optimize
+    },
+    "clara" = {
+      object$metric <- match.arg(metric, c("euclidean", "manhattan", "jaccard"))
+      object$num_samp <- num_samp
+      object$samp_size <- samp_size
+    },
+    stop("'method' should be \"pam\" or \"clara\"")
+  )
+  object$res <- tibble(
+    terms = recipes::sel2char(object$terms),
+    cluster = NA_integer_,
+    selected = NA,
+    silhouette = NA_real_,
+    name = NA_character_
+  )
+
+  structure(object, class = c("step_kmedoids", class(object)))
+
 }
 
 
@@ -211,7 +174,7 @@ tidy.step_kmedoids <- function(x, ...) {
 tunable.step_kmedoids <- function(x, ...) {
   tibble(
     name = "k",
-    call_info = list(list(pkg = "dials", fun = "num_comp", range = c(2, 10))),
+    call_info = list(list(pkg = "dials", fun = "num_comp", range = c(1, 10))),
     source = "MachineShop",
     component = "step_kmedoids",
     component_id = x$id
