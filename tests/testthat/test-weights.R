c6bddea751cec3d5ab5813ef407687f570158e22
Brian J Smith <brian-j-smith@uiowa.edu>
2021-09-05 21:36:03 -0500

    Tests: add skip_if_not() calls to weights testing

--- tests/testthat/test-weights.R
+++ tests/testthat/test-weights.R
@@ -9,6 +9,7 @@ weights <- 1:n
 
 
 test_metrics <- function(obs, pred, weights) {
+  skip_if_not(TEST_ALL)
   metrics <- names(metricinfo(obs, pred))
   perf1 <- performance(obs, pred, metrics = metrics)
   perf2 <- performance(obs, pred, weights, metrics = metrics)
@@ -17,64 +18,53 @@ test_metrics <- function(obs, pred, weights) {
 }
 
 
-test_that("observed factor, predicted factor", {
+test_that("performance comparisons with and without weights", {
+  skip_if_not(TEST_ALL)
+
+  context("Observed factor, Predicted factor")
   obs <- factor(rbinom(n, 2, 0.5))
   pred <- factor(rbinom(n, 2, 0.5))
   res <- test_metrics(obs, pred, weights)
   expect_true(res$pass)
-})
 
-
-test_that("observed factor, predicted matrix", {
+  context("Observed factor, Predicted matrix")
   obs <- factor(rbinom(n, 2, 0.5))
   pred <- matrix(runif(3 * n), ncol = 3)
   pred <- pred / rowSums(pred)
   res <- test_metrics(obs, pred, weights)
   expect_true(res$pass)
-})
 
-
-test_that("observed factor, predicted numeric", {
+  context("Observed factor, Predicted matrix")
   obs <- factor(rbinom(n, 1, 0.5))
   pred <- runif(n)
   res <- test_metrics(obs, pred, weights)
   expect_true(res$pass)
-})
 
-
-test_that("observed matrix, predicted matrix", {
+  context("Observed matrix, Predicted matrix")
   obs <- matrix(rgamma(2 * n, 1, 1), ncol = 2)
   pred <- matrix(rgamma(2 * n, 1, 1), ncol = 2)
   res <- test_metrics(obs, pred, weights)
   expect_true(res$pass)
-})
 
-
-test_that("observed numeric, predicted numeric", {
+  context("Observed numeric, Predicted numeric")
   obs <- rgamma(n, 1, 1)
   pred <- rgamma(n, 1, 1)
   res <- test_metrics(obs, pred, weights)
   expect_true(res$pass)
-})
 
-
-test_that("observed Surv, predicted numeric", {
+  context("Observed Surv, Predicted numeric")
   obs <- Surv(rgamma(n, 1, 1), rbinom(n, 1, 0.5))
   pred <- rgamma(n, 1, 1)
   res <- test_metrics(obs, pred, weights)
   expect_true(res$pass)
-})
 
-
-test_that("observed Surv, predicted SurvEvents", {
+  context("Observed Surv, Predicted SurvEvents")
   obs <- Surv(rgamma(n, 1, 1), rbinom(n, 1, 0.5))
   pred <- SurvEvents(matrix(runif(2 * n), ncol = 2) > 0.5, times = 1:2)
   res <- test_metrics(obs, pred, weights)
   expect_true(res$pass)
-})
 
-
-test_that("observed Surv, predicted SurvProbs", {
+  context("Observed Surv, Predicted SurvProbs")
   obs <- Surv(rgamma(n, 1, 1), rbinom(n, 1, 0.5))
   pred <- SurvProbs(matrix(runif(2 * n), ncol = 2), times = 1:2)
   res <- test_metrics(obs, pred, weights)
